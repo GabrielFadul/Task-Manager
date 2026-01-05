e7f5a05d74792917d8e339d45ac839ec26e024af
Gabriel Fadul <gabrielgfadul022@gmail.com>
2026-01-05 12:41:55 -0300

    feat: Adiciona mapper para conversão de entidades para DTO e DTO para entidades

--- src/main/java/com/gabrielFadul/taskManager/mappers/UserMapper.java
+++ src/main/java/com/gabrielFadul/taskManager/mappers/UserMapper.java
@@ -1,4 +1,22 @@
 package com.gabrielFadul.taskManager.mappers;
 
+import com.gabrielFadul.taskManager.dtos.UserDtoRequest;
+import com.gabrielFadul.taskManager.dtos.UserDtoResponse;
+import com.gabrielFadul.taskManager.models.UserModel;
+
 public class UserMapper {
+
+
+    public UserModel toEntity(UserDtoRequest dtoRequest){
+        UserModel userModel = new UserModel();
+        userModel.setName(dtoRequest.name());
+        userModel.setPassword(dtoRequest.password());
+        userModel.setEmail(dtoRequest.email());
+        return userModel;
+    }
+
+    public UserDtoResponse toResponse(UserModel userModel){
+        return new UserDtoResponse(userModel.getName(), userModel.getEmail());
+    }
+
 }
