4ebf4d4f0c6c20851c0b14d0a5b62755522be829
Gabriel Fadul <gabrielgfadul022@gmail.com>
2026-01-09 17:51:13 -0300

    Foco no backend do projeto

--- frontend/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginPageComponent } from './components/login-page-component/login-page-component';
-
-export const routes: Routes = [
-    { path: 'login', component: LoginPageComponent}
-];
