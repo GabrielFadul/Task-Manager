185f4a1ccf8a5cae97675b409da7c099cb75a7e7
Gabriel Fadul <gabrielgfadul022@gmail.com>
2026-01-09 12:40:11 -0300

    feat: Insere projeto frontend utilizando angular

---
+++ frontend/task-manager-front/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { LoginPageComponent } from './components/login-page-component/login-page-component';
+
+export const routes: Routes = [
+    { path: 'login', component: LoginPageComponent}
+];
